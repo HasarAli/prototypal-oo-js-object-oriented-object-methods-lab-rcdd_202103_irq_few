12708a9597ede54c2751a66a023af85b370dcb5f
Hasar Ali <hasar.ali@protonmail.com>
2021-06-07 01:54:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ BoardMember.prototype.releasePressStatement = function() {
   return 'You will see great things from Scuber'
 }
 
-BoardMember.prototype.sayHi = (wow) => {
-  console.log(wow)
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
-const call = (x) => console.log(this.wow)
-call({wow: 'wow'})
-// new BoardMember('hasar', 'ny', 'web').sayHi({wow: 'wow'})
+new BoardMember('hasar', 'ny', 'web').sayHi({wow: 'wow'})
