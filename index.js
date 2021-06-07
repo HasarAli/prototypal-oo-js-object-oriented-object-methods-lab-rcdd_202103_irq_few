4e7ce688aa36e58f541343e75cb13573f504e449
Hasar Ali <hasar.ali@protonmail.com>
2021-06-07 01:54:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ BoardMember.prototype.sayHi = function() {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 
-new BoardMember('hasar', 'ny', 'web').sayHi({wow: 'wow'})
+new BoardMember('hasar', 'ny', 'web').sayHi()
